836940ad6c830ae3f6976ac83c4f55aead40a496
philipphansen <phenomenal.phil@googlemail.com>
2017-02-24 16:49:42 +0100

    added test for all service methods of actors binaryInput and binaryValue

--- test/pluginTest2.js
+++ test/pluginTest2.js
@@ -25,31 +25,26 @@ describe('[thing-it] BACnet Plugin', function () {
         });
     });
 
-    describe('Binary Input Listener', function () {
+    describe('Binary Input Test Update', function () {
         this.timeout(20000);
 
         before(function () {
             testDriver.removeAllListeners();
         });
-        it('should produce Actor State Change message', function (done) {
-            testDriver.addListener({
-                publishActorStateChange: function (device, actor, state) {
-                    if (actor.id === "binaryInput1" && device.id === "bacnet1" && state.presentValue === true) {
-                        done();
-                    }
-
-                }
-            });
-
-            testDriver.bacnet1.binaryInput1.update();
+        it('should execute the update service successfully', function (done) {
+            testDriver.bacnet1.binaryInput1.update()
+                .then(function() {
+                    done();
+                });
         });
     });
 
-    describe('Binary Value Switch', function () {
+    describe('Binary Value Test ON', function () {
         this.timeout(20000);
 
         before(function () {
             testDriver.removeAllListeners();
+            testDriver.bacnet1.binaryValue1.state.presentValue = false;
         });
         it('should produce Actor State Change message', function (done) {
 
@@ -61,11 +56,80 @@ describe('[thing-it] BACnet Plugin', function () {
 
                 }
             });
-            testDriver.bacnet1.binaryValue1.off()
-                .then(function () {
-                    testDriver.bacnet1.binaryValue1.on();
-                });
 
+            testDriver.bacnet1.binaryValue1.on();
+
+            //testDriver.bacnet1.binaryValue1.off()
+            //    .then(function () {
+            //        testDriver.bacnet1.binaryValue1.on();
+            //    });
+        });
+    });
+
+    describe('Binary Value Test OFF', function () {
+        this.timeout(20000);
+
+        before(function () {
+            testDriver.removeAllListeners();
+            testDriver.bacnet1.binaryValue1.state.presentValue = true;
+        });
+        it('should produce Actor State Change message', function (done) {
+
+            testDriver.addListener({
+                publishActorStateChange: function (device, actor, state) {
+                    if (actor.id === "binaryValue1" && device.id === "bacnet1" && state.presentValue === false) {
+                        done();
+                    }
+
+                }
+            });
+
+            testDriver.bacnet1.binaryValue1.off();
+        });
+    });
+
+    describe('Binary Value Test Toggle OFF/ON', function () {
+        this.timeout(20000);
+
+        before(function () {
+            testDriver.removeAllListeners();
+            testDriver.bacnet1.binaryValue1.state.presentValue = false;
+        });
+        it('should toggle from off to on and produce Actor State Change message', function (done) {
+
+            testDriver.addListener({
+                publishActorStateChange: function (device, actor, state) {
+                    if (actor.id === "binaryValue1" && device.id === "bacnet1" && state.presentValue === true) {
+                        done();
+                    }
+
+                }
+            });
+
+            testDriver.bacnet1.binaryValue1.toggle();
+        });
+    });
+
+    describe('Binary Value Test Toggle ON/OFF', function () {
+        this.timeout(20000);
+
+        before(function () {
+            testDriver.removeAllListeners();
+            testDriver.bacnet1.binaryValue1.state.presentValue = true;
+        });
+
+        it('should toggle from on to off and produce Actor State Change message', function (done) {
+
+            testDriver.addListener({
+                publishActorStateChange: function (device, actor, state) {
+                    if (actor.id === "binaryValue1" && device.id === "bacnet1" && state.presentValue === false) {
+                        done();
+                    }
+
+                }
+            });
+
+            testDriver.bacnet1.binaryValue1.toggle();
         });
     });
 
