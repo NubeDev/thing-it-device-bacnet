25ab898daa39a22d52f15d494de8ebab92212b6c
marcgille <latakia42>
2017-03-06 14:21:12 +0100

    Test skeleton.

--- test/bacNetTestClient.js
+++ test/bacNetTestClient.js
@@ -1,21 +1,32 @@
+var dgram = require('dgram');
 var PORT = 33333;
 var HOST = '127.0.0.1';
 
-var dgram = require('dgram');
-
-var server = dgram.createSocket('udp4');
-
+/**
+ *
+ * @constructor
+ */
 function Adapter() {
 }
 
-Adapter.prototype.initialize = function() {
+/**
+ *
+ * @param host
+ * @param port
+ */
+Adapter.prototype.initialize = function (host, port) {
+    this.TIMEOUT = 10000;
+    this.port = port;
+    this.host = host;
     this.udpServer = dgram.createSocket('udp4');
     this.udpClient = dgram.createSocket('udp4');
 
     this.requests = [];
+    this.listeners = [];
 
     this.udpServer.on('listening', function () {
-        var address = server.address();
+        var address = this.udpServer.address();
+
         console.log('UDP Server listening on ' + address.address + ":" + address.port);
     });
     this.udpServer.on('message', function (message, remote) {
@@ -29,23 +40,42 @@ Adapter.prototype.initialize = function() {
             matchingRequest.resolve(message);
         }
 
-    });
-}
+        for (var n in this.listeners) {
+            if (this.listeners[n] ...) {
+                // TODO Some unmarshalling can happen here
 
-Adapter.prototype.sendXRequest = function() {
+                this.listeners.callback(message);
+            }
+        }
+    });
+
+    this.udpServer.bind(this.port, this.host);
+};
+
+/**
+ * Generic request send
+ *
+ * @param request
+ * @returns {*}
+ */
+Adapter.prototype.sendRequest = function (request) {
     var deferred = q.defer();
-    var request = new Request().initialize(this, deferred);
 
     this.requests.push(request);
 
-    this.udpClient.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+    request.deferred = deferred;
+    request.adapter = this;
+
+    this.udpClient.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
         if (err) {
             _.pull(this.requests, request);
 
             throw err;
         }
 
-        console.log('UDP message sent to ' + HOST +':'+ PORT);
+        request.startWaiting(this.TIMEOUT);
+
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
 
         this.udpClient.close();
     }.bind(this));
@@ -53,23 +83,65 @@ Adapter.prototype.sendXRequest = function() {
     return deferred.promise();
 };
 
+/**
+ *
+ * @returns {*}
+ */
+Adapter.prototype.sendXRequest = function () {
+    var request = new Request().initialize();
 
+    // TODO Some X-specific marshalling happens here
+
+    return this.sendRequest(request);
+};
+
+/**
+ *
+ * @returns {*}
+ */
+Adapter.prototype.sendYRequest = function () {
+    var request = new Request().initialize();
+
+    // TODO Some Y-specific marshalling happens here
+
+    return this.sendRequest(request);
+};
+
+/**
+ *
+ * @constructor
+ */
 function Request() {
 }
 
-Request.prototype.initialize = function(adapter, deferred){
-    this.TIMEOUT = 10000;
-    this.adapter = adapter;
-    this.deferred = deferred;
-    this.timeout = setTimeout(function(){
+/**
+ *
+ * @param adapter
+ * @param deferred
+ */
+Request.prototype.initialize = function () {
+};
+
+/**
+ *
+ * @param message
+ */
+Request.prototype.startWaiting = function (timeout) {
+    this.timeout = setTimeout(function () {
         _.pull(this.adapter.requests, this);
 
         this.deferred.reject('Got a big timeout.');
-    }.bind(this), this.TIMEOUT);
-}
+    }.bind(this), timeout);
+};
 
-Request.prototype.resolve = function(message){
+/**
+ *
+ * @param message
+ */
+Request.prototype.resolve = function (message) {
     clearTimeout(this.timeout);
 
+    _.pull(this.adapter.requests, this);
+
     this.deferred.resolve(message);
-}
+};
