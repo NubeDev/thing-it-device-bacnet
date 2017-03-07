9d636408e0350ef7f837dfdce5a4cc1c66b02b54
philipphansen <phenomenal.phil@googlemail.com>
2017-03-07 17:19:52 +0100

    completed sampled rountrip of a request, response communication in udp

--- test/bacNetTestClient.js
+++ test/bacNetTestClient.js
@@ -1,6 +1,13 @@
 var dgram = require('dgram');
-var PORT = 33333;
-var HOST = '127.0.0.1';
+
+var SERVER_HOST = '127.0.0.1';
+var SERVER_PORT = 33333;
+
+var CLIENT_HOST = '127.0.0.1';
+var CLIENT_PORT = 48651;
+
+var q = require('q');
+var _ = require('underscore');
 
 /**
  *
@@ -10,14 +17,12 @@ function Adapter() {
 }
 
 /**
- *
  * @param host
  * @param port
  */
-Adapter.prototype.initialize = function (host, port) {
+Adapter.prototype.initialize = function () {
+    console.log('adapter initialize');
     this.TIMEOUT = 10000;
-    this.port = port;
-    this.host = host;
     this.udpServer = dgram.createSocket('udp4');
     this.udpClient = dgram.createSocket('udp4');
 
@@ -28,28 +33,53 @@ Adapter.prototype.initialize = function (host, port) {
         var address = this.udpServer.address();
 
         console.log('UDP Server listening on ' + address.address + ":" + address.port);
-    });
+    }.bind(this));
+
     this.udpServer.on('message', function (message, remote) {
         console.log(remote.address + ':' + remote.port + ' - ' + message);
 
-        // Find matching request
+        //check out message
+        console.log(message);
+        console.log(Buffer.isBuffer(message));
+        console.log(Buffer.isEncoding('utf-8'))
 
-        var matchingRequest = _.find(this.requests, {id: message.id});
+        //encodings
+        //console.log(message.toString('hex'));
+        //console.log(message.toString('ascii'));
+        //console.log(message.toString('base64'));
 
-        if (matchingRequest) {
-            matchingRequest.resolve(message);
-        }
+        //buffer -> string -> JSON
+        var messageBody = message.toString('utf-8');
+        console.log(messageBody); //utf-8 is the default
 
-        for (var n in this.listeners) {
-            if (this.listeners[n] ...) {
-                // TODO Some unmarshalling can happen here
+        var messageJSON = JSON.parse(messageBody);
+        console.log(messageJSON);
 
-                this.listeners.callback(message);
+        //building response
+        messageJSON.response = 'me too';
+        responseBody = JSON.stringify(messageJSON);
+        console.log(responseBody);
+
+        //sending response
+        this.udpServer.send(responseBody, 0, responseBody.length, CLIENT_PORT, CLIENT_HOST, function (err, bytes) {
+            if (err) {
+                throw err;
             }
-        }
-    });
 
-    this.udpServer.bind(this.port, this.host);
+            console.log('UDP Server message sent to ' + CLIENT_HOST + ':' + CLIENT_PORT);
+        }.bind(this));
+
+        //for (var n in this.listeners) {
+            //if (this.listeners[n] ...) {
+                //TODO Some unmarshalling can happen here
+
+            //    this.listeners.callback(message);
+            //}
+        //}
+    }.bind(this));
+
+    this.udpServer.bind(SERVER_PORT, SERVER_HOST);
+    this.udpClient.bind(CLIENT_PORT, CLIENT_HOST);
 };
 
 /**
@@ -59,6 +89,7 @@ Adapter.prototype.initialize = function (host, port) {
  * @returns {*}
  */
 Adapter.prototype.sendRequest = function (request) {
+    console.log('adapter send request');
     var deferred = q.defer();
 
     this.requests.push(request);
@@ -66,21 +97,19 @@ Adapter.prototype.sendRequest = function (request) {
     request.deferred = deferred;
     request.adapter = this;
 
-    this.udpClient.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+    this.udpClient.send(request.messageBody, 0, request.messageBody.length, SERVER_PORT, SERVER_HOST, function (err, bytes) {
         if (err) {
-            _.pull(this.requests, request);
+            this.adapter.requests = _.without(this.adapter.requests, this);
 
             throw err;
         }
 
         request.startWaiting(this.TIMEOUT);
 
-        console.log('UDP message sent to ' + HOST + ':' + PORT);
-
-        this.udpClient.close();
+        console.log('UDP Client message sent to ' + SERVER_HOST + ':' + SERVER_PORT);
     }.bind(this));
 
-    return deferred.promise();
+    return deferred.promise;
 };
 
 /**
@@ -88,9 +117,23 @@ Adapter.prototype.sendRequest = function (request) {
  * @returns {*}
  */
 Adapter.prototype.sendXRequest = function () {
-    var request = new Request().initialize();
+    console.log('adapter send x request');
+    //var request = new Request().initialize();
+    var request = new Request();
+    request.message = 'i like tea';
 
     // TODO Some X-specific marshalling happens here
+
+    var body = {
+        time: request.time,
+        message: request.message
+    };
+
+    console.log(body);
+
+    request.messageBody = JSON.stringify(body);
+
+    console.log(request.messageBody);
 
     return this.sendRequest(request);
 };
@@ -99,8 +142,11 @@ Adapter.prototype.sendXRequest = function () {
  *
  * @returns {*}
  */
-Adapter.prototype.sendYRequest = function () {
+Adapter.prototype.sendYRequest = function (message) {
+    console.log('adapter send y request');
+    //var request = new Request().initialize();
     var request = new Request().initialize();
+    request.message = message;
 
     // TODO Some Y-specific marshalling happens here
 
@@ -112,6 +158,8 @@ Adapter.prototype.sendYRequest = function () {
  * @constructor
  */
 function Request() {
+    var d = new Date();
+    this.time = d.getTime();
 }
 
 /**
@@ -127,9 +175,39 @@ Request.prototype.initialize = function () {
  * @param message
  */
 Request.prototype.startWaiting = function (timeout) {
-    this.timeout = setTimeout(function () {
-        _.pull(this.adapter.requests, this);
+    console.log('request start waiting');
+    /*
+    this.adapter.udpClient.on('listening', function () {
+        var address = this.udpClient.address();
 
+        console.log('UDP Client listening on ' + address.address + ":" + address.port);
+    }.bind(this));
+    */
+
+    this.adapter.udpClient.on('message', function (message, remote) {
+        console.log(remote.address + ':' + remote.port + ' - ' + message);
+
+        //buffer -> string -> JSON
+        var messageBody = message.toString('utf-8');
+        console.log(messageBody); //utf-8 is the default
+
+        var messageJSON = JSON.parse(messageBody);
+        console.log(messageJSON);
+
+        //matching back request
+        console.log('try matching message');
+        var matchingRequest = _.find(this.adapter.requests, {time: messageJSON.time});
+
+        if (matchingRequest) {
+            console.log(messageJSON.response);
+            matchingRequest.resolve(message);
+        }
+    }.bind(this));
+
+    this.timeout = setTimeout(function () {
+        //Remove request
+        this.adapter.requests = _.without(this.adapter.requests, this);
+        this.adapter.udpClient.close();
         this.deferred.reject('Got a big timeout.');
     }.bind(this), timeout);
 };
@@ -139,9 +217,17 @@ Request.prototype.startWaiting = function (timeout) {
  * @param message
  */
 Request.prototype.resolve = function (message) {
+    console.log('request resolve');
     clearTimeout(this.timeout);
-
-    _.pull(this.adapter.requests, this);
-
+    this.adapter.requests = _.without(this.adapter.requests, this);
+    this.adapter.udpClient.close();
+    this.adapter.udpServer.close();
     this.deferred.resolve(message);
 };
+
+//TEST
+
+var testAdapter = new Adapter();
+testAdapter.initialize(SERVER_HOST,SERVER_PORT);
+
+testAdapter.sendXRequest();
